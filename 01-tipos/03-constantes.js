8d8cc1a994b019b74a9cd60a103b12b801821085
DevAriana <ariana.calderon.20isc@tecsanpedro.edu.mx>
2024-01-20 14:17:50 -0600

    constantes

---
+++ 01-tipos/03-constantes.js
@@ -0,0 +1,6 @@
+let nombre="Hola mundo";
+nombre="ArianaaaaA";
+console.log(nombre);
+/*Las variables pueden cambiar su valor en el futuro 
+Pero es preferente que no cambien, si se quiere otro valor es mejor crear otra variable*/
+const name="Ariana";/*Para asegurar que las variables no cambiarán su valor */
