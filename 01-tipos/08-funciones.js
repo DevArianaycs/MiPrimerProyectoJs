b219b1f324729e6477c0527b0469038c73cf8dbd
DevAriana <ariana.calderon.20isc@tecsanpedro.edu.mx>
2024-01-20 16:20:24 -0600

    funciones

---
+++ 01-tipos/08-funciones.js
@@ -0,0 +1,14 @@
+function saludar() { //creación de una función
+    console.log("hola mundo");
+}
+//las funciones se ejecutan cuando se mandan llamar en el código
+
+//MANDAR LLAMAR
+saludar();
+
+//función para operaciones
+function suma(){
+    return 2+2;
+}
+
+console.log(suma()); //imprimir el resultado que arroja la función
