556930f5412af849e3c2e8599764f0ad7602f6ea
DevAriana <ariana.calderon.20isc@tecsanpedro.edu.mx>
2024-01-21 16:20:24 -0600

    operadores de asignación

---
+++ 02-operadores/02-asignacion.js
@@ -0,0 +1,12 @@
+let a=5;
+a=a+5;
+a=a++;
+//si se quiere incrementar con mas de 1 (no usar ++)
+a+=5; //aumenta de 5 en 5
+//y con los demás operadores
+a-=5;
+a*=5;
+a/=5;
+a%=5;
+a**=5;
+console.log(a);
