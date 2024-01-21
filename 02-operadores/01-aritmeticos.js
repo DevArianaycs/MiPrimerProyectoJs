2db85f5776a3ff2d00194e05e205b5764bfae0f2
DevAriana <ariana.calderon.20isc@tecsanpedro.edu.mx>
2024-01-21 16:14:47 -0600

    operadores aritmeticos

---
+++ 02-operadores/01-aritmeticos.js
@@ -0,0 +1,16 @@
+let a=5;
+let b=7;
+//operadores:
+// console.log(a+b, ' suma'); //suma
+// console.log(a-b,' resta'); //resta
+// console.log(a*b,' multiplicación'); //multiplicación
+// console.log(a/b,' división'); //división
+// console.log(a%b,' módulo'); //módulo, o lo que sobra de una división
+// console.log(a**b, ' potencia'); //potencia (a elevado a b)
+
+//incrementar
+console.log(a++);
+console.log(a);
+//decrementar
+console.log(a--);
+console.log(a);
