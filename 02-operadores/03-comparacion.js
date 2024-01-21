1f890db82ace52c2dc4f7c20e6da339347b52b40
DevAriana <ariana.calderon.20isc@tecsanpedro.edu.mx>
2024-01-21 16:30:07 -0600

    operadores comparación

---
+++ 02-operadores/03-comparacion.js
@@ -0,0 +1,13 @@
+//relacionales
+let a=10;
+console.log(a>5);
+console.log(a>=5);
+console.log(a<5);
+console.log(a<=5);
+
+//operadores igualdad
+console.log(a==10);
+console.log(a!=10);
+console.log(a=="10"); //== solo pregunta por el valor, no por el tipo, por eso me da un true
+console.log(a==="10"); //pregunta por si es el mismo valor Y el mismo tipo de dato
+console.log(a!=="10"); //pregunta si son distinto tipo de dato Y distinto valor
