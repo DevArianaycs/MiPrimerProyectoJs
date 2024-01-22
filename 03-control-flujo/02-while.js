95f5006c50535c4b2dad69cad76151c95fe2097f
DevAriana <ariana.calderon.20isc@tecsanpedro.edu.mx>
2024-01-22 14:19:43 -0600

    while

---
+++ 03-control-flujo/02-while.js
@@ -0,0 +1,12 @@
+//cuales son los numeros pares
+let i=0;
+while(i<10){
+    if(i%2==0){
+        console.log(i,'es in número par');
+    }
+    else{
+    console.log(i);
+    }
+    i++;
+}
+console.log('fuera del while');
