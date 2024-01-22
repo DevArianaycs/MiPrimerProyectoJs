bf42c139b0da50850b2a2dbd3ccf9d32c7d50bf7
DevAriana <ariana.calderon.20isc@tecsanpedro.edu.mx>
2024-01-21 22:35:17 -0600

    if-else

---
+++ 03-control-flujo/01-if.js
@@ -0,0 +1,13 @@
+let edad=10;
+if(edad>17){
+    console.log('usuario mayor de edad');
+}
+else if(edad>13){
+    console.log('Usuario necesita conpañía de un adulto');
+}
+else{
+    console.log('No puede ingresar');
+}
+
+
+   
