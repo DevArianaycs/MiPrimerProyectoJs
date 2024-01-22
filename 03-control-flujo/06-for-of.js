fac2aab6bdca9efa0529dae66996f22ff4ede886
DevAriana <ariana.calderon.20isc@tecsanpedro.edu.mx>
2024-01-22 17:29:16 -0600

    for of

---
+++ 03-control-flujo/06-for-of.js
@@ -0,0 +1,11 @@
+ //elementos a iterar
+ let animales =['gatito guero','gatito borrao','gatito pirata'];
+ for (let gatitos of animales){
+    console.log(gatitos);
+ }
+ //otra forma de hacerlo con el while
+let i=0;
+ while(i<animales.length){
+   console.log(animales[i]);
+   i++;
+ }
