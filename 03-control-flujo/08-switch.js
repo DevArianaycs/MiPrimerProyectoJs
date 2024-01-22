c466183a5a3d969a93bd0ac1f5586285ffe3c00b
DevAriana <ariana.calderon.20isc@tecsanpedro.edu.mx>
2024-01-22 17:53:51 -0600

    switch

---
+++ 03-control-flujo/08-switch.js
@@ -0,0 +1,12 @@
+let accion='listar';
+switch (accion){
+    case 'listar':
+        console.log('Acción de listar');
+        break;
+    case 'guardar':
+        console.log('Acción de guardar');
+        break;
+    default: console.log('acción no reconocida'); //siempre que no exista la opción del case
+
+
+}
