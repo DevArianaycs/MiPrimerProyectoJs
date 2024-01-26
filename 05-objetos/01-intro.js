ab8bac62e5901b4758f8c46946a6d064154ec00b
DevAriana <ariana.calderon.20isc@tecsanpedro.edu.mx>
2024-01-25 22:29:34 -0600

    intro

---
+++ 05-objetos/01-intro.js
@@ -0,0 +1,17 @@
+let email='arianaycs15@gmail.com'
+let name="Ariana Calderon"
+let direccion={
+    calle:"nomsa",
+    numero:12,
+};
+
+let user={
+    email:"ariana.io",
+    name:"Ari",
+    direccion:{
+        calle:"jand",
+        numero:14,
+    },
+    activo:true,
+    recuperarClave:function recuperarClave(){console.log('recuperando clave');} //función anónima, para que no sea redundante
+};
